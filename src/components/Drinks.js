f79076608a288e8c1be1269eb911a5cfc3f32166
Jiaxi <dev.jiaxiwu@gmail.com>
2020-05-18 12:11:14 -0400

    h1 tags

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-import Margaritas from './Margaritas';
-import Cocktails from './Cocktails'
+import React, { Component } from "react";
+import Margaritas from "./Margaritas";
+import Cocktails from "./Cocktails";
 
 class Drinks extends Component {
-    state = {  }
-    render() { 
-        return ( <div>
-            <Margaritas />
-            <Cocktails />
-        </div> );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Margaritas</h1>
+        <Margaritas />
+        <h1> Cocktails </h1>
+        <Cocktails />
+      </div>
+    );
+  }
 }
- 
+
 export default Drinks;
