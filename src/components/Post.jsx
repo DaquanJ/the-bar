abc21958fa5f7216b06b788c53ea342dfe0575da
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-05-18 23:06:07 -0400

    establishing state as database

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,16 +4,25 @@ import React, { Component } from 'react';
 class Post extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            userRecipes: [
+                {name: ''},
+                {images: []}, 
+                {ingredients: ''}
+            ]
+         }
     }
+
     render() { 
         return ( 
             <div>
                 <h1> I am the posting feature! </h1>
                 <form >
-                    <input type="text"/>
-                    <input type="image"/>
-                    <input type="text"/>
+                    <h3>Name</h3>
+                    <input type="text" onChange={ e => {this.setState({name: e.target.value})} } />
+                    <input type="image" alt=''/>
+                    <h3>ingredients</h3>
+                    <input type="submit" value='+'/>
                 </form>
             </div>
          );
