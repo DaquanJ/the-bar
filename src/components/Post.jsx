b67e8784a972d1d6ccf59a68ab096fd0eae5c6e7
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-05-19 00:34:47 -0400

    establishing conditional rendering feature

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,30 +9,38 @@ class Post extends Component {
                 {name: ''},
                 {images: []}, 
                 {ingredients: ''}
-            ]
+            ],
+            cocktails: false
          }
     }
 
     
     render() { 
+        const cocktailCreator = () => {
+            if (this.state.cocktails === false) {
+                return null;
+            }
+            return (
+                this.state.userRecipes.map(cocktail => ( 
+                    <div>
+                        <h3> {cocktail.name} </h3>
+                        <img src={cocktail.images} alt="images"/>
+                        <p> {cocktail.ingredients} </p>
+                    </div>
+                    ))
+                    )
+        }
         return ( 
             <div>
-            {this.state.userRecipes.map(cocktail => ( 
-                <div>
-                    <h3> {cocktail.name} </h3>
-                    <img src={cocktail.images} alt=""/>
-                    <p> {cocktail.ingredients} </p>
-                </div>
-                ))}
                 <h1> I am the posting feature! </h1>
                 <form onSubmit = {e => e.preventDefault()} >
                     <h3>Name</h3>
                     <input type="text" onChange={ e => {this.setState({name: e.target.value})} } />
                     <input type="image" alt=''/>
                     <h3>ingredients</h3>
-                    <input type="submit" value='+' />
-                    
+                    <input type="submit" value='+' onClick = {() => this.setState({cocktails: true})} />
                 </form>
+                {cocktailCreator()}
             </div>
          );
     }
