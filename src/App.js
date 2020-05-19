1098bf6702441e84ba58c1f275985c9fb00d4bb4
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-05-19 17:56:06 -0400

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
 import React, { Component } from 'react';
+import Header from "./components/Header";
 import Drinks from "./components/Drinks";
 import BlueMargarita from "./components/BlueMargarita";
 import Margarita from "./components/Margarita";
