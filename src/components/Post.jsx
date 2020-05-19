33f52fa355d2ff6cae025aa9558273cb361885a1
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-05-19 00:25:25 -0400

    establishing mapping configs

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,16 +13,25 @@ class Post extends Component {
          }
     }
 
+    
     render() { 
         return ( 
             <div>
+            {this.state.userRecipes.map(cocktail => ( 
+                <div>
+                    <h3> {cocktail.name} </h3>
+                    <img src={cocktail.images} alt=""/>
+                    <p> {cocktail.ingredients} </p>
+                </div>
+                ))}
                 <h1> I am the posting feature! </h1>
-                <form >
+                <form onSubmit = {e => e.preventDefault()} >
                     <h3>Name</h3>
                     <input type="text" onChange={ e => {this.setState({name: e.target.value})} } />
                     <input type="image" alt=''/>
                     <h3>ingredients</h3>
-                    <input type="submit" value='+'/>
+                    <input type="submit" value='+' />
+                    
                 </form>
             </div>
          );
